dfacf858df79431d0d91edbd0aef6311edeaaa76
Florian-Cywinski <florian.cywinski@protonmail.com>
2024-03-19 19:16:51 +0100

    IdeaList Component

--- client/src/components/IdeaList.js
+++ client/src/components/IdeaList.js
@@ -1,7 +1,7 @@
 class IdeaList {
   constructor() {
-    this._ideaListEl = document.querySelector('#idea-list');
-    this._ideas = [
+    this._ideaListEl = document.querySelector('#idea-list');  // The selector where the cards will be inserted
+    this._ideas = [ // To initialize an array for the ideas, later the data fetched from the DB will be added there // For now we use harcoded data
       {
         id: 1,
         text: 'Idea 1',
@@ -16,8 +16,8 @@ class IdeaList {
         username: 'Jill',
         date: '02/01/2023',
       },
-    ];
-    this._validTags = new Set();
+    ];  // To have for each tag / category a different color
+    this._validTags = new Set();  // A Set is a collection of unique values. Each value can only occur once in a Set. A Set can hold any value of any data type.
     this._validTags.add('technology');
     this._validTags.add('software');
     this._validTags.add('business');
@@ -26,21 +26,22 @@ class IdeaList {
     this._validTags.add('inventions');
   }
 
-  getTagClass(tag) {
+  getTagClass(tag) {  // To get the correct tag - To have for each tag / category a different color
     tag = tag.toLowerCase();
-    let tagClass = '';
-    if (this._validTags.has(tag)) {
-      tagClass = `tag-${tag}`;
+    let tagClass = '';  // To initialize the variable tagClass
+    if (this._validTags.has(tag)) { // To check whether the tag is part of the set
+      tagClass = `tag-${tag}`;  // e.g. .tag-technology
     } else {
-      tagClass = '';
+      tagClass = '';  // The default color is black
     }
     return tagClass;
   }
 
-  render() {
+  render() {  // To create / render HTML code (the idea cards)
     this._ideaListEl.innerHTML = this._ideas
-      .map((idea) => {
-        const tagClass = this.getTagClass(idea.tag);
+      .map((idea) => {  // To loop through the ideas array
+        const tagClass = this.getTagClass(idea.tag);  // To get the correct tag / category e.g. technology
+        // To put each idea as HTML code into the output array 
         return `
       <div class="card">
       <button class="delete"><i class="fas fa-times"></i></button>
@@ -55,8 +56,8 @@ class IdeaList {
     </div>
       `;
       })
-      .join('');
+      .join('');  // To turn the array (the map output with the HTML code for each idea card) back into a string
   }
 }
 
-export default IdeaList;
+export default IdeaList;  // The import is implemented in index.js
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,11 @@
 import '@fortawesome/fontawesome-free/css/all.css';
-import Modal from './components/Modal'; // Ti import the Modal class (constructor)
-import IdeaForm from './components/IdeaForm';
+import Modal from './components/Modal';         // To import the Modal class (constructor)
+import IdeaForm from './components/IdeaForm';   // To import the IdeaForm class (constructor)
+import IdeaList from './components/IdeaList';   // To import the IdeaList class (constructor)
 import './css/style.css';
 
 const modal = new Modal();  // To instanciate the modal
 const ideaForm = new IdeaForm();    // To instanciate the idea form
 ideaForm.render();  // To call the method render on this object
+const ideaList = new IdeaList();    // To instanciate the idea list
+ideaList.render();  // It's just rendered here
