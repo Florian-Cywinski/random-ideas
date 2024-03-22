9b04d342089e971859a2c90e74b187deb1c9925d
Florian-Cywinski <florian.cywinski@protonmail.com>
2024-03-22 10:49:15 +0100

    Delete Ideas

--- client/src/components/IdeaList.js
+++ client/src/components/IdeaList.js
@@ -16,13 +16,35 @@ class IdeaList {
     this._validTags.add('inventions');
   }
 
-  async getIdeas() {  // To get the data - From the imported IdeasApi which fetches all ideas from the DB
+  addEventListeners() {
+    this._ideaListEl.addEventListener('click', (e) => {
+      if (e.target.classList.contains('fa-times')) {      // fa-times is the class of the delete button (x)
+        e.stopImmediatePropagation(); //  The stopImmediatePropagation() method of the Event interface prevents other listeners of the same event from being called. If several listeners are attached to the same element for the same event type, they are called in the order in which they were added. If stopImmediatePropagation() is invoked during one such call, no remaining listeners will be called, either on that element or any other element.
+        const ideaId = e.target.parentElement.parentElement.dataset.id; // <button class="delete"> . <div class="card" data-id="${idea._id}"> // data-id we access with dataset -> the attribute is id
+        this.deleteIdea(ideaId);  // To delete it from the server and from the DOM
+      }
+    });
+  }
+
+  async getIdeas() {  // To get the data - From the imported IdeasApi which fetches all ideas from the DB // async because axios returns a promise
     try {
       const res = await IdeasApi.getIdeas();
       this._ideas = res.data.data;
       this.render();  // To render the list - make it visible on the DOM
     } catch (error) {
       console.log(error);
+    }
+  }
+
+  async deleteIdea(ideaId) {  // async because axios returns a promise
+    try {
+      // Delete from server
+      const res = await IdeasApi.deleteIdea(ideaId);
+      // Delete from DOM
+      this._ideas.filter((idea) => idea._id !== ideaId);  // To filter it out from the _ideas array -> give all back but the one deleted
+      this.getIdeas();  // To update the DOM (render again)
+    } catch (error) {
+      alert('You can not delete this resource');
     }
   }
 
@@ -46,11 +68,15 @@ class IdeaList {
     this._ideaListEl.innerHTML = this._ideas
       .map((idea) => {  // To loop through the ideas array
         const tagClass = this.getTagClass(idea.tag);  // To get the correct tag / category e.g. technology
+        const deleteBtn =
+          idea.username === localStorage.getItem('username')
+            ? `<button class="delete"><i class="fas fa-times"></i></button>`  // if the username is in local storage the delete icon is there
+            : ''; // if not there is no delete icon
         // To put each idea as HTML code into the output array 
         return `
-      <div class="card">
-      <button class="delete"><i class="fas fa-times"></i></button>
-      <h3>
+      <div class="card" data-id="${idea._id}">
+      ${deleteBtn}
+        <h3>
         ${idea.text}
       </h3>
       <p class="tag ${tagClass}">${idea.tag.toUpperCase()}</p>
@@ -62,6 +88,7 @@ class IdeaList {
       `;
       })
       .join('');  // To turn the array (the map output with the HTML code for each idea card) back into a string
+    this.addEventListeners(); // Normaly it would goes into the constructor but in this case it has to come in after rendering
   }
 }
 
